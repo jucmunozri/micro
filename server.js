a8e95b87cb56bf91464acf1e9811456610fe6d5d
jucmunozri <jucmunozri@gmail.com>
2022-12-03 03:57:01 -0500

    fix load-v7

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const { mongoConn } = require("./databases/configuration");
 const dotenv = require("dotenv").config();
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3001);
 // app.set("json spaces", 2);
 
 const conn = mongoConn();
